b11bd36be8127f62dad77b36bf0b70670fad7a80
hopsm.dev <hopsm.dev@gmail.com>
2015-12-13 11:31:24 +0100

    twitter small refactoring

--- twitter.py
+++ twitter.py
@@ -23,56 +23,58 @@ class Tweets(object):
         return "{date}: {text}".format(
             date=self._tweet.created_at, text=self._tweet.text)
 
+
+class TweetsUser(Tweets):
+    def __init__(self, api):
+        self.api = api
+        super(TweetsUser, self).__init__(api)
+
     def __call__(self, user_name):
         for obj in self.api.user_timeline(screen_name=user_name, count=200):
             yield self._tweet(str(obj.created_at), obj.text.encode('utf8'))
 
 
-class TweetsHashtags(object):
+class TweetsHashtag(Tweets):
     def __init__(self, api):
         self.api = api
-        self._tweet = namedtuple('tweets', ['created_at', 'text'])
+        super(TweetsHashtag, self).__init__(api)
 
-    def __str__(self):
-        return "{date}: {text}".format(
-            date=self._tweet.created_at, text=self._tweet.text)
-
-    def __call__(self, hashtag):
-
+    def __call__(self, hashtag, since):
         search_params = {
-            'q': hashtag,
-            'since': '2015-12-12',
-            'lang': 'en',
-            'rpp':20}
+            'q': hashtag, 'since': since, 'lang': 'en'}
 
         for obj in tweepy.Cursor(self.api.search, **search_params).items(30):
             yield self._tweet(str(obj.created_at), obj.text.encode('utf8'))
 
 
-credentials = CredentialsReader('credentials.ini')
-__api_key = credentials.twitter.api_key
-__api_secret_key = credentials.twitter.api_secret_key
-__access_token = credentials.twitter.access_token
-__access_secret = credentials.twitter.access_secret
+if __name__ == "__main__":
 
-api = get_twitter_api(auth=get_0auth(
-    __api_key, __api_secret_key, __access_token, __access_secret))
+    credentials = CredentialsReader('credentials.ini')
+    __api_key = credentials.twitter.api_key
+    __api_secret_key = credentials.twitter.api_secret_key
+    __access_token = credentials.twitter.access_token
+    __access_secret = credentials.twitter.access_secret
+
+    api = get_twitter_api(auth=get_0auth(
+        __api_key, __api_secret_key, __access_token, __access_secret))
 
 
-def print_user_tweets(users):
-    tweets = Tweets(api=api)
-    for user in users:
-        for tweet in tweets(user):
-            print(user, tweet)
+    def print_user_tweets(users):
+        tweets = TweetsUser(api=api)
+        for user in users:
+            for tweet in tweets(user):
+                print(user, tweet)
 
-users = ['gvanrossum', 'raymondh']
-#print_user_tweets(users)
+    users = ['gvanrossum', 'raymondh']
+    #print_user_tweets(users)
 
-def print_hashtag_tweets(hashtags):
-    twitter_hashtag = TweetsHashtags(api=api)
-    for hashtag in hashtags:
-        for tweet in twitter_hashtag(hashtag):
-            print(hashtag, tweet)
+    def print_hashtag_tweets(hashtags, since):
+        tweets = TweetsHashtag(api=api)
+        for hashtag in hashtags:
+            for tweet in tweets(hashtag, since):
+                print(hashtag, tweet)
 
-hashtags = ['python']
-print_hashtag_tweets(hashtags)
+    hashtags = ['#python']
+
+    from datetime import date
+    print_hashtag_tweets(hashtags, since=str(date.today()))
