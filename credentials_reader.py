353d550d8eac63d3dda4220ed5550ba1395ec1da
hopsm.dev <hopsm.dev@gmail.com>
2015-12-18 21:33:24 +0100

    remove property attr and add __getattrs__ method insted

--- credentials_reader.py
+++ credentials_reader.py
@@ -3,18 +3,29 @@ from collections import namedtuple
 
 
 class CredentialsReader(object):
-    def __init__(self, credentials_path):
+    """
+    How to use:
+    credentials_reader = CredentialsReader(credentials_path)
+    credentials_reader.twitter.api_key
+
+    Attributes for object generated according to sections
+    in credentials.ini file - you should add attribute name
+    to __attrs__ list.
+    """
+
+    __attrs__ = ['twitter']
+
+    def __init__(self, credentials_path='../credentials.ini'):
         self.config = configparser.ConfigParser()
         self.config.read(credentials_path)
 
-    @property
-    def twitter(self):
-        _twitter_section = 'twitter'
-        try:
-            _items = self.config[_twitter_section].items()
-            twitter = namedtuple(
-                'twitter', self.config.options(_twitter_section))
-            return twitter(**{key: value for (key, value) in _items})
-        except configparser.NoSectionError:
-            raise RuntimeError('Cannot find twitter credentials in credentials.ini')
-
+    def __getattr__(self, item):
+        if item in self.__attrs__:
+            _section = item
+            try:
+                _items = self.config[_section].items()
+                _attribute = namedtuple(
+                    _section, self.config.options(_section))
+                return _attribute(**{key: value for (key, value) in _items})
+            except configparser.NoSectionError:
+                raise RuntimeError('Cannot find {} credentials in credentials.ini'.format(_section))
--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,6 @@
 import unittest
+import twitter
 from credentials_reader import CredentialsReader
-
 
 class CredentialsReaderTest(unittest.TestCase):
 
@@ -11,6 +11,26 @@ class CredentialsReaderTest(unittest.TestCase):
     def test_twitter(self):
         print(self.credentials_reader.twitter.api_key)
         print(self.credentials_reader.twitter.api_secret_key)
+
+    def test_attr_noexist(self):
+        self.assertEqual(self.credentials_reader.noexists, None)
+
+
+class GetTweetsTest(unittest.TestCase):
+
+    def setUp(self):
+        credentials = CredentialsReader()
+        self.__api_key = credentials.twitter.api_key
+        self.__api_secret_key = credentials.twitter.api_secret_key
+        self.__access_token = credentials.twitter.access_token
+        self.__access_secret = credentials.twitter.access_secret
+
+    def test_oauth(self):
+        auth = twitter.get_0auth(
+            self.__api_key,
+            self.__api_secret_key,
+            self.__access_token,
+            self.__access_secret)
 
 
 if __name__ == "__main__":
