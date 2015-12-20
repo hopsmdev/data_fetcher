14dcf615f390a81b453f3e6cc15c8f28058d6c0d
hopsm.dev <hopsm.dev@gmail.com>
2015-12-20 22:11:42 +0100

    add since parameter to user tweets

--- data_fetcher/twitter.py
+++ data_fetcher/twitter.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from collections import namedtuple
 
 import tweepy
@@ -37,9 +38,25 @@ class TweetsUser(Tweets):
         self.api = api
         super(TweetsUser, self).__init__(api)
 
-    def __call__(self, user_name):
-        for obj in self.api.user_timeline(screen_name=user_name, count=200):
-            yield self._tweet(str(obj.created_at), obj.text.encode('utf8'))
+    def __call__(self, user, number_of_tweets=20, since=None):
+        """
+        :param user: string, twitter screen name
+        :param number_of_tweets: integer
+        :param since: datetime.datetime type e.g
+            datetime.datetime(2015, 12, 14, 16, 34, 33) or
+            datetime.datetime.today()
+        :return: tweepy Status object generator
+        """
+        for obj in tweepy.Cursor(
+                self.api.user_timeline,
+                screen_name=user).items(number_of_tweets):
+            if since:
+                if obj.created_at >= since:
+                    yield self._tweet(str(obj.created_at), obj.text.encode('utf8'))
+                else:
+                    continue
+            else:
+                yield self._tweet(str(obj.created_at), obj.text.encode('utf8'))
 
 
 class TweetsHashtag(Tweets):
@@ -47,18 +64,17 @@ class TweetsHashtag(Tweets):
         self.api = api
         super(TweetsHashtag, self).__init__(api)
 
-    def __call__(self, hashtag, since):
+    def __call__(self, hashtag, since, number_of_tweets=20):
         search_params = {
             'q': hashtag, 'since': since, 'lang': 'en'}
 
-        for obj in tweepy.Cursor(self.api.search, **search_params).items(30):
+        for obj in tweepy.Cursor(
+                self.api.search, **search_params).items(number_of_tweets):
             yield self._tweet(str(obj.created_at), obj.text.encode('utf8'))
 
 
 class TwitterDataFetcher(object):
-    def __init__(self, twitter_config_obj, auth_obj=None):
-        self.users_to_observe = twitter_config_obj.users.split(',')
-        self.tags_to_observe = twitter_config_obj.hashtags.split(',')
+    def __init__(self, auth_obj=None):
 
         if auth_obj:
             self.auth = auth_obj
@@ -76,30 +92,32 @@ class TwitterDataFetcher(object):
             access_token=_credentials.twitter.access_token,
             access_secret=_credentials.twitter.access_secret)
 
-    def get_hashtag_tweets(self, hashtags=None, since=None):
+    def get_hashtag_tweets(self, hashtag=None, since=None):
         _tweets = TweetsHashtag(api=self.api)
-        if self.tags_to_observe:
-            hashtags = self.tags_to_observe
-        for hashtag in hashtags:
-            for _tweet in tweets(hashtag, since):
+        for _tweet in _tweets(hashtag, since):
                 yield _tweet
 
-    def get_user_tweets(self, users=None):
+    def get_user_tweets(self, user, number_of_tweets, since):
         _tweets = TweetsUser(api=self.api)
-        if self.users_to_observe:
-            users = self.users_to_observe
-        for user in users:
-            for _tweet in tweets(user):
-                yield _tweet
+        for _tweet in _tweets(
+                user=user, number_of_tweets=number_of_tweets, since=since):
+            yield _tweet
 
 
 if __name__ == "__main__":
 
     twitter_data_config = get_twitter_data_config(
         data_configs="data_configs", config_ini="twitter.ini")
-    twitter_data_fetcher = TwitterDataFetcher(twitter_data_config)
+
+    users_to_observe = twitter_data_config.users.split(',')
+    tags_to_observe = twitter_data_config.hashtags.split(',')
+
+    twitter_data_fetcher = TwitterDataFetcher()
 
     from datetime import date
-    tweets = twitter_data_fetcher.get_hashtag_tweets(since=str(date.today()))
+    #tweets = twitter_data_fetcher.get_hashtag_tweets(since=str(date.today()))
+    tweets = twitter_data_fetcher.get_user_tweets(
+        user="gvanrossum", since=datetime.datetime(2015, 12, 14, 16, 34, 33))
+
     for tweet in tweets:
         print(tweet)
