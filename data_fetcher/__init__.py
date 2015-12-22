5940c456bb8fa8c08b6084661a3ca9fdba71c87b
hopsm.dev <hopsm.dev@gmail.com>
2015-12-22 21:33:59 +0100

    add logger

--- data_fetcher/__init__.py
+++ data_fetcher/__init__.py
@@ -1,7 +1,20 @@
 import os
-import sys
 import inspect
+import logging
 
 CURRENT_DIR = os.path.dirname(
     os.path.abspath(
         inspect.getfile(inspect.currentframe())))
+
+
+def _setup_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    stream_handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
+
+
+logger = _setup_logger()
