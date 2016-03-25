87cd4d6ddaff26bf6b1edd126854af5d7af26d00
hopsmdev <hopsmdev@gmail.com>
2016-03-25 17:05:36 +0100

    rename data_fetcher to twitter, small refactor to improve look&fell after long break

--- data_fetcher/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import inspect
-
-CURRENT_DIR = os.path.dirname(
-    os.path.abspath(
-        inspect.getfile(inspect.currentframe())))
-
-sys.path.append(os.path.abspath(os.path.join(CURRENT_DIR, os.pardir)))
